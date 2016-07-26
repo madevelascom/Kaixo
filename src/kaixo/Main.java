3e001b08827fcae2e9a597166b093188d46b2c25
mvelasco93 <m_velasco93@hotmail.com>
2016-07-26 18:30:04 -0500

    Corrections
    Removed unnecessary files, login window corrected, controllers and fxmls
    unified in same packages, css defined (WIP).

--- src/kaixo/Main.java
+++ src/kaixo/Main.java
@@ -59,7 +59,7 @@ public class Main extends Application {
     
     public void showKaixoInterface() throws IOException{
         FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(Main.class.getResource("KaixoInterface.fxml"));
+        loader.setLocation(Main.class.getResource("GUI/login.fxml"));
         KaixoInterface = (BorderPane) loader.load();
 
         rootLayout.setCenter(KaixoInterface); 
